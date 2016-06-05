a6c638646cfb54a1fd2adc671cde7e05ff6bda16
Radek <radek@rzaluska.com>
2016-06-05 17:24:37 +0200

    Fix border cases in thresholding

--- canny.s
+++ canny.s
@@ -66,18 +66,26 @@ canny:
     push R13
     push R14
     push R15
+    mov rax, rcx
+    mov R15, 16
+    mov R14, RDX
+    mov RDX, 0
+    div R15
+    mov R15, rax ; number of chunks in row
+    xchg R14, rdx ; reminder in row
     xor R12, R12; i = 0
+    dec rdx ; ommit last row
     .for_i_in_rows:
         cmp R12, rdx ; i < heigth
         jnb .exit
 
         xor R13, R13; j = 0
         .for_j_in_columns:
-            cmp R13, rcx; j < width
+            cmp R13, R15; j < width
             jnb .exit_inner_loop
 
             MOVDQU xmm1, [rdi]; current pixel
-            MOVDQU xmm2, [rdi+rcx+ 1]; x+1 y+1
+            MOVDQU xmm2, [rdi+rcx+1]; x+1 y+1
 
             PSUBSB xmm1, xmm2
             PABSB xmm1, xmm1
@@ -93,14 +101,11 @@ canny:
             ; save 16 pixels
             MOVDQU [rsi], xmm3
 
-            add R13, 16 ; move to next 16 pixels
+            add R13, 1; move to next chunk
             add rdi, 16
             add rsi, 16
             jmp .for_j_in_columns
         .exit_inner_loop:
-        sub R13, rcx
-        sub rdi, R13
-        sub rsi, R13
         inc R12
         jmp .for_i_in_rows
 .exit:
@@ -245,11 +250,25 @@ thresholding:
 blur_assembly:
     push RBP
     push rbx
+    push r9
     push R12
     push R13
     push R14
     push R15
-    xor R12, R12; i = 0
+
+    mov rax, rcx
+    mov R15, 16
+    mov R14, RDX
+    mov RDX, 0
+    div R15
+    mov R15, rax ; number of chunks in row
+    xchg R14, rdx ; reminder in row
+    mov r9, r14
+    test r14, r14
+    jnz .not_sub_one_form_chunks
+    dec R15
+    mov r9, 16
+    .not_sub_one_form_chunks:
 
     mov rax, 63
     MOVQ xmm6, rax
@@ -261,14 +280,16 @@ blur_assembly:
     PXOR xmm7, xmm7
     PSHUFB xmm5, xmm7 ;10000000
 
+    xor R12, R12; i = 0
+    dec rdx
     .for_i_in_rows:
         cmp R12, rdx ; i < heigth
-        jnb .exit
+        jnb .last_row
 
         xor R13, R13; j = 0
         .for_j_in_columns:
-            cmp R13, rcx; j < width
-            jnb .exit_inner_loop
+            cmp R13, R15; j < width
+            jnb .reminder
             PXOR xmm7, xmm7
 
             MOVDQU xmm1, [rdi]; current pixel
@@ -299,21 +320,56 @@ blur_assembly:
             PXOR xmm7, xmm5
             MOVDQU [rsi], xmm7
 
-            add R13, 16; move to next 16 pixels
+            add R13, 1; move to chunk
             add rdi, 16
             add rsi, 16
             jmp .for_j_in_columns
+            .reminder:
+                mov R13, r9
+                xor R14, R14; i = 0
+                xor rax, rax
+                .for_i_in_reminder:
+                    cmp R14, R13, ; i < reminder
+                    jnb .exit_inner_loop
+                    MOV al, [rdi]
+                    MOV [rsi], al
+                    inc rdi
+                    inc rsi
+                    inc R14
+                    jmp .for_i_in_reminder
         .exit_inner_loop:
-        sub R13, rcx
-        sub rdi, R13
-        sub rsi, R13
         inc R12
         jmp .for_i_in_rows
+    .last_row:
+        xor R13, R13; j = 0
+        .last_row_for_j_in_columns:
+            cmp R13, R15; j < width
+            jnb .last_reminder
+            MOVDQU xmm1, [rdi]; current pixel
+            MOVDQU [rsi], xmm1; current pixel
+            add rdi, 16
+            add rsi, 16
+            inc R13
+            jmp .last_row_for_j_in_columns
+        .last_reminder:
+                mov R13, r9
+                xor R14, R14; i = 0
+                .last_reminder_for_i_in_reminder:
+                    cmp R14, R13, ; i < reminder
+                    jnb .exit
+                    MOV al, [rdi]
+                    MOV [rsi], al
+                    inc rdi
+                    inc rsi
+                    inc R14
+                    jmp .last_reminder_for_i_in_reminder
+
 .exit:
     pop R15
     pop R14
     pop R13
     pop R12
+    pop r9
     pop rbx
     pop rbp
     ret
